e1169485e501309ff6c4d59f789742c8afb4299c
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2020-05-29 08:29:59 +0800

    Delete repetitive code

--- Exercises/exer8/src/exer8_q124_helper.m
+++ Exercises/exer8/src/exer8_q124_helper.m
@@ -21,6 +21,8 @@ beta=round(r2/a2);
 plot(alpha,beta,'m*',x(1),y(1),'m*')
 msg = sprintf('E(%d, %d)',alpha,beta);
 text(alpha-10, beta+20, msg);
+msg = sprintf('Initial Point(%d, %d)', x(1),y(1));
+text(x(1)-20, y(1)+20, msg);
 for k=1:years
    x(k+1)=(1+r1-a1*y(k))*x(k);
     % in prictice terms, # Of rats & owls are non-negative
@@ -37,21 +39,11 @@ for k=1:years
         y(k+1)=0;
     end
     
-    % draw vecters (with arrow) every two points
-    
+    % draw vecters (with arrow) every two points    
     hold on
     plot(x(k),y(k),'b.', x(k+1),y(k+1),'b.')
     vectarrow([x(k),y(k)],[x(k+1),y(k+1)])
 end
-% let E=(alpha, beta) be the equilibrium point
-% let x_{k+1}=x_k, y{k+1}=y_k, then solve them
-alpha=r1/a1;
-beta=r2/a2;
-hold on
-plot(alpha,beta,'m*')
-msg = sprintf('Initial Point(%d, %d)', x(1),y(1));
-text(x(1)-20, y(1)+20, msg);
-
 hold on
 plot(x(years+1),y(years+1),'m*')
 msg = sprintf('End Point(%d, %d)', x(years+1),y(years+1));
